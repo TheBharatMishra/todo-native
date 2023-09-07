10716b691cde24438dcbb547506d2bf8601586a0
TheBharatMishra <mishrabm2004@gmail.com>
2023-09-08 01:31:01 +0530

    Some Updates

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 // import './wind.config.js';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
 <script src="https://cdn.tailwindcss.com"></script>;
 
