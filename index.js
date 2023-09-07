24fcfd0be25cd441a443aaaa3a243ee4fdebd39b
TheBharatMishra <mishrabm2004@gmail.com>
2023-09-07 14:15:43 +0530

    Some updates

--- index.js
+++ index.js
@@ -5,5 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+<script src="https://cdn.tailwindcss.com"></script>;
 
 AppRegistry.registerComponent(appName, () => App);
